3e861d27ac25f46aa0cf67e0e1f01bea809f361e
Alexandre Santos <alexandre.santozz@gmail.com>
2020-06-09 00:48:24 +0100

    fix: newsletter text

--- src/components/subscribe-to-newsletter/index.js
+++ src/components/subscribe-to-newsletter/index.js
@@ -28,13 +28,13 @@ const SubscribeToNewsletter = () => {
   return (
     <div>
       <hr />
-      <h2>Subscribe to my newsletter to get updates on my new articles!</h2>
+      <h2>Subscribe to my newsletter to get updates on my newest articles!</h2>
       <p>
-        I will{" "}
+        No worries, I will{" "}
         <span className={styles.bold}>
           not send more than 1 email per month
         </span>{" "}
-        , nor sell your email to third parties, no worries 😉
+        , nor sell your email to third parties.
       </p>
       {isSuccess ? (
         <p>Thank you, I appreciate it! 🙌</p>
